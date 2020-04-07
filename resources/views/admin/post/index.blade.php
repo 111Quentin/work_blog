3b674165ac4ab5403a33fb7ee0383fb1549979c7
111Quentin <1551881447@qq.com>
2020-04-07 20:40:46 +0800

    初始化博客页面

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>博客首页</title>
-</head>
-<body>
-    <h1>文章页面</h1>
-</body>
-</html>
+@extends("admin.layout.main")
+
+@section("content")
+    <h1>试试水</h1>
+@endsection
