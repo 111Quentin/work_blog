29db7719e368a24e36593a1832ea604f4568043d
111Quentin <1551881447@qq.com>
2020-04-08 21:12:00 +0800

    修正语言风格

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -7,41 +7,28 @@ use App\Http\Controllers\Controller;
 use App\Model\Admin\Post;
 use App\Model\Admin\User;
 use App\Model\Admin\PostLog;
+use Auth;
 use DB;
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        $user = \Auth::user()->toArray();
-        if($user['name'] == 'admin'){
-            $posts = Post::where('id','>',0)->orderBy('created_at', 'desc')->paginate(5);
-        }else{
-            $posts = Post::where('user_id',$user['id'])->orderBy('created_at', 'desc')->paginate(5);
+        $user = Auth::user()->toArray();
+        if ( $user['name'] == 'admin' ) {
+            $posts = Post::where('id', '>', 0)->orderBy('created_at', 'desc')->paginate(5);
+        } else {
+            $posts = Post::where('user_id', $user['id'])->orderBy('created_at', 'desc')->paginate(5);
         }
-        return view('admin.post.index',compact('posts'));
+        return view('admin.post.index', compact('posts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('admin.post.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -50,51 +37,38 @@ class PostController extends Controller
             'content' => 'required|min:10',
         ]);
         //获取登录用户信息
-        $user   = User::where('id',\Auth::id())->first()->toArray();
-        $params = array_merge(request(['title','desc' ,'content']), ['user_id' => \Auth::id(),'author' => $user['name'],'created_at' => date("Y-m-d H:i:s",time())]);
+        $user   = User::where('id', Auth::id())->first()->toArray();
+        $params = array_merge(request(['title', 'desc' ,'content']), ['user_id' => Auth::id(), 'author' => $user['name'], 'created_at' => date("Y-m-d H:i:s", time())]);
         $post   = Post::create($params)->toArray();
         //入库日志
         if($post){
-            $data = array();
-            $data['post_id'] = $post['id'];
-            $data['user_id'] = \Auth::id();
-            $data['action'] = 'insert';
-            $data['title'] = request(['title'][0]);
-            $data['desc'] = request(['desc'][0]);
-            $data['content'] = request(['content'][0]);
-            $data['ip'] = $_SERVER['REMOTE_ADDR'];
-            $data['create_time'] = time();
+            $data                   =  array();
+            $data['post_id']        =  $post['id'];
+            $data['user_id']        =  Auth::id();
+            $data['action']         =  'insert';
+            $data['title']          =  request(['title'][0]);
+            $data['desc']           =  request(['desc'][0]);
+            $data['content']        =  request(['content'][0]);
+            $data['ip']             =  $_SERVER['REMOTE_ADDR'];
+            $data['create_time']    =  time();
             PostLog::insert($data);
         }
         return redirect('/posts');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Post $post)
     {
         return view('admin.post.show', compact('post'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Post $post)
     {
         return view('admin.post.edit', compact('post'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Post $post)
     {
         $this->validate($request, [
@@ -103,8 +77,8 @@ class PostController extends Controller
             'content' => 'required|min:10',
         ]);
         //获取登录用户信息
-        $user   = User::where('id',\Auth::id())->first()->toArray();
-        $params = array_merge(request(['title','desc' ,'content']), ['user_id' => \Auth::id(),'author' => $user['name'],'updated_at' => date("Y-m-d H:i:s",time())]);
+        $user   = User::where('id', Auth::id())->first()->toArray();
+        $params = array_merge(request(['title', 'desc', 'content']), ['user_id' => Auth::id(), 'author' => $user['name'], 'updated_at' => date("Y-m-d H:i:s", time())]);
         $this->authorize('update', $post);
         $res = $post->update($params);
         //写入日志
@@ -123,12 +97,7 @@ class PostController extends Controller
         return redirect("/posts/{$post->id}");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Post $post)
     {
         $this->authorize('delete', $post);
@@ -150,16 +119,17 @@ class PostController extends Controller
      * 搜索
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function search(){
-        $this->validate(request(),[
+    public function search()
+    {
+        $this->validate(request(), [
             'query' => 'required'
         ]);
         $query = request('query');
-        if(preg_match('/\d+/',$query)){
-            $posts = Post::where('created_at','like','%'.$query.'%')->paginate(10);
+        if(preg_match('/\d+/', $query)){
+            $posts = Post::where('created_at', 'like', '%' . $query . '%')->paginate(10);
         }
         else{
-            $posts = Post::where('title','like','%'.$query.'%')->orWhere('author','like','%'.$query.'%')->paginate(10);
+            $posts = Post::where('title', 'like', '%' . $query . '%')->orWhere('author', 'like' , '%' . $query . '%')->paginate(10);
         }
         return view('admin.post.search', compact('posts', 'query'));
     }
