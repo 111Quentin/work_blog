69daae2a1758144ff4c38f50a8d5680defdbf20f
111Quentin <1551881447@qq.com>
2020-04-09 17:08:36 +0800

    重构注册模块

--- app/Http/Controllers/Admin/RegisterController.php
+++ app/Http/Controllers/Admin/RegisterController.php
@@ -1,8 +1,9 @@
 <?php
 namespace  App\Http\Controllers\Admin;
-use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
 use App\Model\Admin\User;
+use App\Http\Requests\RegisterPost;
 
 class RegisterController extends Controller
 {
@@ -12,30 +13,9 @@ class RegisterController extends Controller
         return view('admin.register.index');
     }
     //注册
-    public function register()
+    public function register(RegisterPost $request)
     {
-        $this->validate(request() ,[
-            'name'              => 'required|min:3|unique:users,name',
-            'email'             => 'required|unique:users,email|email',
-            'password'          => 'required|min:6|max:30|confirmed',
-            'captcha'           => 'required|captcha'
-        ]);
-
-        $data                   = array();
-        $data['password']       = bcrypt(request('password'));
-        $data['name']           = request('name');
-        $data['email']          = request('email');
-        $data['ip']             = $_SERVER['REMOTE_ADDR'];
-        $data['created_at']     = date("Y-m-d H:i:s",time());
-
-        //1每次客户端新增判断一下是否有ip存在，而且是否1分钟内再注册
-        $ip = User::where('ip', '=', $data['ip'])->orderBy('created_at', 'desc')->first();
-        if (isset($ip) && time() - strtotime($ip->toArray()['created_at']) < 60) {
-            return redirect('/admin/register')->withErrors([
-                'registerError' => '同个ip1分钟内只能注册一个账号'
-            ]);
-        }
-        $res = User::insert($data);
+        $res = (new User())->register();
         if ($res) {
             return redirect('/admin/login');
         } else {
--- app/Model/Admin/User.php
+++ app/Model/Admin/User.php
@@ -1,53 +1,78 @@
 <?php
-    namespace  App\Model\Admin;
-    use Illuminate\Database\Eloquent\Model;
-    //引入trait
-    use Illuminate\Auth\Authenticatable;
+namespace  App\Model\Admin;
 
-    class User extends  Model implements \Illuminate\Contracts\Auth\Authenticatable{
-        // 指定与当前模块关联的数据表
-        protected $table = 'users';
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Auth;
 
-        public $timestamps = true;
-        // 填充字段
-        protected $fillable= [
-            'name', 'email', 'password','ip'
-        ];
-        protected $hidden = [
-            'password', 'remember_token',
-        ];
+class User extends  Model implements \Illuminate\Contracts\Auth\Authenticatable
+{
+    // 指定与当前模块关联的数据表
+    protected $table = 'users';
 
-        // 使用trait,相当于将整个trait代码复制过来(trait是php 5.4才有的语法,主要用于实现代码复用)
-        use Authenticatable;
+    public $timestamps = true;
 
-        /**
-         * 检查登录
-         */
-        public function checkLogin($user = array()){
-            $data                   =   array();
-            $data['email']          =   $user['email'];
-            $data['ip']             =   $_SERVER['REMOTE_ADDR'];
-            $data['create_time']    =   time();
+    // 填充字段
+    protected $fillable= [
+        'name', 'email', 'password','ip'
+    ];
 
-            //检查是否有此用户
-            $userInfo = self::where('email',$user['email'])->first();
-            //统计登录错误次数
-            $logMins = time() - 10 * 60;
-            $loginCounts = LoginLog::where('email',$user['email'])->where('create_time','>=',$logMins)->count();
-            //登录成功
-            if(true == \Auth::guard('web')->attempt($user) && $loginCounts < 5){
-                //记录登录成功日志
-                $data['login_type'] = 'suc';
-                LoginLog::insert($data);
-                return 1;
-            }else if(empty($userInfo)){  //无此用户
-                return 0;
-            }else if($loginCounts >= 5){
-                return -1;
-            }else{
-                $data['login_type'] = 'fail';
-                LoginLog::insert($data);
-                return -2;
-            }
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // 使用trait,相当于将整个trait代码复制过来(trait是php 5.4才有的语法,主要用于实现代码复用)
+    use Authenticatable;
+
+    /**
+     * 注册
+     */
+    public function  register(){
+        $data                   = array();
+        $data['password']       = bcrypt(request('password'));
+        $data['name']           = request('name');
+        $data['email']          = request('email');
+        $data['ip']             = $_SERVER['REMOTE_ADDR'];
+        $data['created_at']     = date("Y-m-d H:i:s", time());
+
+        //每次客户端新增判断一下是否有ip存在，而且是否1分钟内再注册
+        $ip = self::where('ip', '=', $data['ip'])->orderBy('created_at', 'desc')->first();
+        if (isset($ip) && time() - strtotime($ip->toArray()['created_at']) < 60) {
+            return redirect('/admin/register')->withErrors([
+                'registerError' => '同个ip1分钟内只能注册一个账号'
+            ]);
+        }
+        return User::insert($data);
+    }
+
+    /**
+     * 检查登录
+     */
+    public function checkLogin($user = array()){
+        $data                   =   array();
+        $data['email']          =   $user['email'];
+        $data['ip']             =   $_SERVER['REMOTE_ADDR'];
+        $data['create_time']    =   time();
+
+        //检查是否有此用户
+        $userInfo = self::where('email', $user['email'])->first();
+        //统计登录错误次数
+        $logMins = time() - 10 * 60;
+        $loginCounts = LoginLog::where('email', $user['email'])->where('create_time', '>=', $logMins)->count();
+        //登录成功
+        if (true == Auth::guard('web')->attempt($user) && $loginCounts < 5) {
+            //记录登录成功日志
+            $data['login_type'] = 'suc';
+            LoginLog::insert($data);
+            return 1;
+        } else if(empty($userInfo)) {  //无此用户
+            return 0;
+        } else if($loginCounts >= 5) {
+            return -1;
+        } else {
+            $data['login_type'] = 'fail';
+            LoginLog::insert($data);
+            return -2;
         }
     }
+}
